dea61f6b7e136f99344345918e219cebc6695678
Saurabh Kumar <rajsaurabh78@gmail.com>
2023-02-23 11:25:37 +0530

    add

--- Customer_Service_Center/src/main/java/com/customer/Service/CustomerService.java
+++ Customer_Service_Center/src/main/java/com/customer/Service/CustomerService.java
@@ -1,9 +1,14 @@
 package com.customer.Service;
 
 import com.customer.Entity.Customer;
+import com.customer.Exception.CustomerException;
 
 public interface CustomerService {
 	
 	public Customer registerCustomer(Customer customer);
-
+	
+	public String loginCustomer(Login login) throws CustomerException;
+	public String changePassword(Login login) throws CustomerException;
+	public String forgetPassword(Integer id) throws CustomerException;
+	public Customer emailPassword(Integer id) throws CustomerException;
 }
