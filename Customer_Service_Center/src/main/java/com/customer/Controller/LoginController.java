8ddc4a971390731da2591623fba8b7fb4d55d784
Rajni Kant Arya <101392769+rkmasai@users.noreply.github.com>
2023-02-24 06:44:21 +0530

    Login

--- Customer_Service_Center/src/main/java/com/customer/Controller/LoginController.java
+++ Customer_Service_Center/src/main/java/com/customer/Controller/LoginController.java
@@ -10,13 +10,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.customer.Entity.Login;
 import com.customer.Service.LoginService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
 public class LoginController {
 	
